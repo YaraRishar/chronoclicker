c52c9ec5765dbfa5bbe2da176d004c58bbbcfbe8
YaraRishar <fenella5864@mail.ru>
2026-02-11 21:58:24 +0300

    добавлен установщик

---
+++ chronoclicker_installer.bat
@@ -0,0 +1,43 @@
+SETLOCAL
+SET "INSTALLER=%TEMP%\python-3.13.2-amd64.exe"
+powershell -WindowStyle Hidden -Command "Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force; Get-ExecutionPolicy | Out-File -Encoding ASCII %TEMP%\sep.txt"
+SET /P SEPVAR=<%TEMP%\sep.txt
+
+ECHO Checking for Python installation...
+%LOCALAPPDATA%\Programs\Python\Python313\python.exe --version >NUL 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO Python is already installed.
+    SET "PYTHON_PATH=%LOCALAPPDATA%\Programs\Python\Python313\python.exe"
+) ELSE (
+    ECHO Python is not installed. Proceeding with installation...
+
+    powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.13.2/python-3.13.2-amd64.exe -OutFile %INSTALLER% | Out-File -Encoding ASCII %TEMP%\pydl.txt"
+
+    SET /P PYDLVAR=<%TEMP%\pydl.txt
+
+    IF NOT EXIST %INSTALLER% (
+        ECHO Installer download failed or file not found.
+        PAUSE
+        EXIT /B 1
+    )
+    START /WAIT %INSTALLER% /quiet InstallAllUsers=0 PrependPath=0 TargetDir=%LOCALAPPDATA%\Programs\Python\Python313
+
+    IF EXIST "%LOCALAPPDATA%\Programs\Python\Python313\python.exe" (
+        SET "PYTHON_PATH=%LOCALAPPDATA%\Programs\Python\Python313\python.exe"
+    ) ELSE (
+        ECHO Unable to locate Python executable.
+        PAUSE
+        EXIT /B 1
+    )
+    SETX PATH "%LOCALAPPDATA%\Programs\Python\Python313;%LOCALAPPDATA%\Programs\Python\Python313\Scripts;%PATH%"
+)
+"%PYTHON_PATH%" -V
+
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri https://github.com/YaraRishar/chronoclicker/archive/refs/heads/main.zip -OutFile %TEMP%\chronoclicker-main.zip"
+mkdir C:\Users\%USERNAME%\Downloads\chronoclicker
+powershell Expand-Archive chronoclicker-main.zip -DestinationPath C:\Users\%USERNAME%\Downloads\chronoclicker
+cd C:\Users\%USERNAME%\Downloads\chronoclicker
+python -m venv .venv
+.venv\Scripts\activate.bat
+pip install -r requirements.txt
+python main.py
